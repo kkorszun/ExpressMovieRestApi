e30299917112ca133d0d270bc1cac8cd44c19212
Kacper <kacperkorszun@gmail.com>
2018-11-03 11:45:31 +0100

    Add eslint automation

--- app.js
+++ app.js
@@ -1,87 +1,87 @@
-const express = require('express')
-var bodyParser = require('body-parser')
-const app = express()
-const port = 3000
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var mockDb = [];
+const app = express();
+const port = 3000;
 
-var mockIdGen = (function(){
-    var counter = 0;
-    return function() {
-        counter += 1;
-        return counter;
-    }
-})();
+const mockDb = [];
+
+const mockIdGen = (function () {
+  let counter = 0;
+  return function () {
+    counter += 1;
+    return counter;
+  };
+}());
 
 function getComments(id, callback) {
-    callback(null, [])
+  callback(null, []);
 }
 
 function getAllComments(callback) {
-    callback(null, [])
+  callback(null, []);
 }
 
 function addMovie(title, callback) {
-    const movieObj = {id : mockIdGen(),  movie: { title : title}};
-    mockDb.push(movieObj);
-    callback(null, movieObj);
+  const movieObj = { id: mockIdGen(), movie: { title } };
+  mockDb.push(movieObj);
+  callback(null, movieObj);
 }
 
 function getAllMovies(callback) {
-    callback(null, mockDb);
+  callback(null, mockDb);
 }
 
-function getMovie(id, callback) {
-    const result = mockDb.filter(x => x.id == id);
-    callback(null, result);
-}
+/* function getMovie(id, callback) {
+  const result = mockDb.filter(x => x.id === id);
+  callback(null, result);
+} */
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/movies', (req, res) => getAllMovies((err,data) => {
-    if(err) {
-        res.sendStatus(500);
-    } else{
-        res.json(data);
-    }
-}))
+app.get('/movies', (req, res) => getAllMovies((err, data) => {
+  if (err) {
+    res.sendStatus(500);
+  } else {
+    res.json(data);
+  }
+}));
 
 app.get('/comments/:id(\\d+)', (req, res) => {
-    getComments(req.body.title, (err, data) => {
-        if(err) {
-            //...
-        } else res.json(data)
-    })
-})
+  getComments(req.body.title, (err, data) => {
+    if (err) {
+      // ...
+    } else res.json(data);
+  });
+});
 
-app.get('/comments', (req, res) =>{
-    getAllComments((err, data) => {
-        if(err) { /* ... */} else res.json(data)
-    })
-})
+app.get('/comments', (req, res) => {
+  getAllComments((err, data) => {
+    if (err) { /* ... */ } else res.json(data);
+  });
+});
 
 app.post('/movies', (req, res) => {
-    if(req.body.title) {
-        addMovie(req.body.title, (err, data) => {
-            if(err) {
-                res.sendStatus(500)
-            } else {
-                res.json(data)
-            }
-        }) 
-        
-    } else{
-        res.sendStatus(400)
-    }    
-})
+  if (req.body.title) {
+    addMovie(req.body.title, (err, data) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.json(data);
+      }
+    });
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 app.post('/comments', (req, res) => {
-    if(req.body.id && req.body.comment) {
-        res.json(req.body)
-    } else{
-        res.sendStatus(400)
-    }    
-})
+  if (req.body.id && req.body.comment) {
+    res.json(req.body);
+  } else {
+    res.sendStatus(400);
+  }
+});
 
 
-app.listen(port, () => console.log(`App listening on port ${port}!`))
+app.listen(port, () => console.log(`App listening on port ${port}!`));
