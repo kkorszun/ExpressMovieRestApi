07fce7359ba2336220764ee0bddebcc76eb6205e
Kacper <kacperkorszun@gmail.com>
2018-11-07 16:04:51 +0100

    create movieService

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-console */
 const express = require('express');
 const bodyParser = require('body-parser');
-const myHttpPromise = require('./myHttpPromise');
 require('dotenv').config();
 const db = require('./db');
 
@@ -23,6 +22,18 @@ function parseObjectId(id) {
     return id;
   }
   return undefined;
+}
+
+function parseComment(reqBody) {
+  let movie_id;
+  let text;
+  if (reqBody.movie_id && typeof reqBody.movie_id === 'string') {
+    movie_id = parseObjectId(reqBody.movie_id);
+  }
+  if (reqBody.text && typeof reqBody.text === 'string') {
+    text = parseObjectId(reqBody.movie_id);
+  }
+  return { movie_id, text };
 }
 
 // --- error handlers
@@ -55,6 +66,7 @@ const port = process.env.PORT || 3000;
 db.connect();
 const Movie = mongoose.model('Movie', { movie: Object });
 const Comment = mongoose.model('Comment', { movie_id: String, text: String });
+const movieService = require('./services/movie')(Movie);
 // ---
 
 mongoose.set('bufferCommands', false);
@@ -67,37 +79,11 @@ function getAllComments(callback) {
   Comment.find(callback);
 }
 
-function addMovie(title, callback) {
-  const newTitle = title.trim().split(' ').filter(x => x !== '').join('+');
-  const myUrl = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${newTitle}`;
-  let movie;
-  myHttpPromise(myUrl)
-    .then((data) => {
-      movie = JSON.parse(data.toString());
-      if (movie.Response && movie.Response === 'False') {
-        return Promise.reject(new Error(movie.Error));
-      }
-      return Movie.findOne({ movie }).exec();
-    })
-    .then((movie2) => {
-      if (!movie2) {
-        return Movie.create({ movie });
-      }
-      return Promise.reject(new Error('Movie already exist'));
-    })
-    .then(result => callback(null, result))
-    .catch(error => callback(error));
-}
-
 function addComment(movie_id, text, callback) {
   Movie.findById(movie_id).exec()
     .then(() => Comment.create({ movie_id, text }))
     .then(comment => callback(null, comment))
     .catch(callback);
-}
-
-function getAllMovies(callback) {
-  Movie.find(callback);
 }
 
 /* function getMovie(id, callback) {
@@ -114,7 +100,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('/movies', (req, res, next) => getAllMovies((err, data) => {
+app.get('/movies', (req, res, next) => movieService.getAll((err, data) => {
   if (err) {
     // res.sendStatus(500);
     next(err);
@@ -122,6 +108,19 @@ app.get('/movies', (req, res, next) => getAllMovies((err, data) => {
     res.json(data);
   }
 }));
+
+app.post('/movies', (req, res, next) => {
+  const title = parseTitle(req.body);
+  if (title) {
+    movieService.add(title, (err, data) => {
+      if (err) { next(err); } else {
+        res.json(data);
+      }
+    });
+  } else {
+    next(new Error('Value is not notempty String'));
+  }
+});
 
 app.get('/comments/:id', (req, res, next) => {
   const id = parseObjectId(req.params.id);
@@ -141,31 +140,6 @@ app.get('/comments', (req, res, next) => {
     if (err) { next(err); } else res.json(data);
   });
 });
-
-app.post('/movies', (req, res, next) => {
-  const title = parseTitle(req.body);
-  if (title) {
-    addMovie(title, (err, data) => {
-      if (err) { next(err); } else {
-        res.json(data);
-      }
-    });
-  } else {
-    next(new Error('Value is not notempty String'));
-  }
-});
-
-function parseComment(reqBody) {
-  let movie_id;
-  let text;
-  if (reqBody.movie_id && typeof reqBody.movie_id === 'string') {
-    movie_id = parseObjectId(reqBody.movie_id);
-  }
-  if (reqBody.text && typeof reqBody.text === 'string') {
-    text = parseObjectId(reqBody.movie_id);
-  }
-  return { movie_id, text };
-}
 
 app.post('/comments', (req, res, next) => {
   const { movie_id, text } = parseComment(req.body);
---
+++ services/movie.js
@@ -0,0 +1,32 @@
+const myHttpPromise = require('../myHttpPromise');
+
+function add(title, callback) {
+  const newTitle = title.trim().split(' ').filter(x => x !== '').join('+');
+  const myUrl = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${newTitle}`;
+  let movie;
+  myHttpPromise(myUrl)
+    .then((data) => {
+      movie = JSON.parse(data.toString());
+      if (movie.Response && movie.Response === 'False') {
+        return Promise.reject(new Error(movie.Error));
+      }
+      return this.Movie.findOne({ movie }).exec();
+    })
+    .then((movie2) => {
+      if (!movie2) {
+        return this.Movie.create({ movie });
+      }
+      return Promise.reject(new Error('Movie already exist'));
+    })
+    .then(result => callback(null, result))
+    .catch(error => callback(error));
+}
+
+function getAll(callback) {
+  this.Movie.find(callback);
+}
+
+// eslint-disable-next-line func-names
+module.exports = function (Movie) {
+  return { Movie, add, getAll };
+};
