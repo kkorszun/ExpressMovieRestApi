2e47c9dda630376541dccd3c8211b76020d36641
Kacper <kacperkorszun@gmail.com>
2018-11-20 12:42:56 +0100

    use axios package for rest fetching

--- src/services/movie.js
+++ src/services/movie.js
@@ -1,12 +1,14 @@
-const myHttpPromise = require('../myHttpPromise');
+const axios = require('axios');
+// const myHttpPromise = require('../myHttpPromise');
 const Movie = require('../models/movie');
 
 const add = async (title, callback) => {
-  const newTitle = title.trim().split(' ').filter(x => x !== '').join('+');
-  const myUrl = `http://www.omdbapi.com/?apikey=${process.env.API_KEY}&t=${newTitle}`;
+  const t = title.trim().split(' ').filter(x => x !== '').join('+');
+  const myUrl = 'http://www.omdbapi.com/';
+  const params = { apikey: process.env.API_KEY, t };
   try {
-    const data = await myHttpPromise(myUrl);
-    const movie = JSON.parse(data.toString());
+    const data = await axios.get(myUrl, { params });
+    const movie = data.data;
     if (movie.Response && movie.Response === 'False') {
       throw new Error(movie.Error);
     }
